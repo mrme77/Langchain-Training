40139386ce229e811e8729fbc463678bf343f91c
mrme77 <psalomone33@gmail.com>
2025-05-04 15:22:28 -0500

    adds

---
+++ 3_chains/3_chains_gemini.py
@@ -0,0 +1,36 @@
+import os
+import time
+import sys
+#print(os.environ["GOOGLE_API_KEY"])
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+time.sleep(10)
+model = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0.3,
+    max_tokens=200,
+    timeout=None,
+    max_retries=1    # other params...
+)
+
+# Define prompt templates (no need for separate Runnable chains)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a facts expert who knows facts about {animal}."),
+        ("human", "Tell me {fact_count} facts."),
+    ]
+)
+
+# Create the combined chain using LangChain Expression Language (LCEL)
+chain = prompt_template | model | StrOutputParser()
+# chain = prompt_template | model
+
+# Run the chain
+result = chain.invoke({"animal": "elephant", "fact_count": 1})
+
+# Output
+print(result)
--- 3_chains/3_chains_ollama.py
+++ 3_chains/3_chains_ollama.py
@@ -1,13 +1,11 @@
-from dotenv import load_dotenv
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
 from langchain.schema.output_parser import StrOutputParser
-from langchain_openai import ChatOpenAI
 
-# Load environment variables from .env
-load_dotenv()
 
-# Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+system_message = SystemMessage(content="You are a helpful AI assistant.")
+model = OllamaLLM(model="gemma3:12b-it-qat", temperature=0.9, max_tokens=200, timeout=None, max_retries=1)
 
 # Define prompt templates (no need for separate Runnable chains)
 prompt_template = ChatPromptTemplate.from_messages(
@@ -18,7 +16,7 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 
 # Create the combined chain using LangChain Expression Language (LCEL)
-chain = prompt_template | model #| StrOutputParser()
+chain = prompt_template | model | StrOutputParser()
 # chain = prompt_template | model
 
 # Run the chain
