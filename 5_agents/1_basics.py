c39a5d9917f1b0c38f090b16049b63056af6f068
mrme77 <psalomone33@gmail.com>
2025-05-07 21:58:54 -0500

    edits

--- 5_agents/1_basics.py
+++ 5_agents/1_basics.py
@@ -1,13 +1,15 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
+#from langchain_openai import ChatOpenAI
+from langchain_ollama.llms import OllamaLLM
+#from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain import hub
 from langchain.agents import create_react_agent, AgentExecutor
 import datetime
 from langchain.agents import tool
+import sys
 
-load_dotenv()
+#load_dotenv()
 
 @tool
 def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
@@ -17,18 +19,23 @@ def get_system_time(format: str = "%Y-%m-%d %H:%M:%S"):
     formatted_time = current_time.strftime(format)
     return formatted_time
 
-llm = ChatOpenAI(model="gpt-4")
+#llm = ChatOpenAI(model="gpt-4")
+llm = OllamaLLM(model="gemma3:12b-it-qat", temperature=0.9, max_tokens=200, timeout=None, max_retries=1)
 
-query = "What is the current time in London? (You are in India). Just show the current time and not the date"
+
+query = "What time is in Napoli,Italy? (You are in Illinois). Just show the current time and not the date"
 
 prompt_template = hub.pull("hwchase17/react")
-
 tools = [get_system_time]
+
 
 agent = create_react_agent(llm, tools, prompt_template)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-agent_executor.invoke({"input": query})
+
+
+result = agent_executor.invoke({"input": query})
+#print(result)
 
 
